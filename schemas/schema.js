2507e8487421c56efc89e748edb3259173b03aa9
Igor Napierala <igornapierala@gmail.com>
2021-08-28 09:46:05 +0200

    Update schema.js

--- schemas/schema.js
+++ schemas/schema.js
@@ -19,13 +19,15 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    /* documents */
     property,
+    person,
+    
+    /* types */
     propertyImage,
     review,
     traveller,
     host,
-    person,
-    
+
   ]),
 });
